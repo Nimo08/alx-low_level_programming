38ecac14d7daa6f7e744deb49eeb07cc00af954d
Nimo08 <nimomohamed800@gmail.com>
2023-04-20 02:30:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*Task 4: using puts()*/
+/**
+ * main - Entry Point
+ * Description: demonstrate using puts() function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
