d1de08c73fceb237a45dd79909649636ce717dca
Nimo08 <nimomohamed800@gmail.com>
2023-04-20 02:19:05 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-
+/*Task 4: using puts()*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
