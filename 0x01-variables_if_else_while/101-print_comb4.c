32fff049139fc02aaf9a18cb4e2d0ab2497869ce
Nimo08 <nimomohamed800@gmail.com>
2023-04-24 07:08:56 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all possible different combinations
+ * of three digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 7; x++)
+	{
+		for(y = x + 1; y <= 8; y++)
+		{
+			for(z = y + 1; z <= 9; z++)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				putchar('0' + z);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
