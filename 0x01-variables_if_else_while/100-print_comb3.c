6bc4038a58f9a33d04bae02f2868390dfc6fa427
Nimo08 <nimomohamed800@gmail.com>
2023-04-24 06:48:28 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of
+ * 2 digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	while (x < 100)
+	{
+		putchar('0' + (x / 10));
+		putchar('0' + (x % 10));
+		x++;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
