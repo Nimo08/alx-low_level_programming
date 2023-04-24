7e526a4b53a0a36c5601b312ca384f62180de70a
Nimo08 <nimomohamed800@gmail.com>
2023-04-24 02:25:29 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print single digit numbers of base 10
+ * starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar('0' + x);
+	}
+	putchar('\n');
+	return (0);
+}
