76e0e018429daaf4169522640d5691b70e044973
Nimo08 <nimomohamed800@gmail.com>
2023-04-25 08:31:32 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @x: integer to be added
+ * @y: integer to be added
+ * Description: adds two ints and returns result
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	int n;
+
+	n = x + y;
+	return (n);
+}
