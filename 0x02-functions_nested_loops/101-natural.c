596ef73c82a0f4cb737db35a26974980625c0f61
Nimo08 <nimomohamed800@gmail.com>
2023-04-25 10:10:53 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**main - entry point
+ * Return - Always 0
+ */
+int main(void)
+{
+	int i, j  = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+		i++;
+		
+	}
+	printf("%d\n", j);
+	return (0);
+}
