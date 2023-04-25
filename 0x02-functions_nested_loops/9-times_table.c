15784b94f851eadc0709d595d53946c57bc2a33a
Nimo08 <nimomohamed800@gmail.com>
2023-04-25 08:55:59 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 				_putchar(k + '0');
 
 			}
-			if ( k < 10 && j != 0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -33,7 +33,6 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			
 		}
 		_putchar('\n');
 	}
