e67d1aaa6d9c379ca77d9abec266b2175a978d6c
Nimo08 <nimomohamed800@gmail.com>
2023-04-25 11:52:49 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(' ');
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
@@ -30,7 +30,6 @@ void print_times_table(int n)
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
