5a4571d77f527b750df53fc8d7e99155469b789b
Nimo08 <nimomohamed800@gmail.com>
2023-04-27 04:13:25 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,26 @@
 /**
  * print_line - draws straight line in the terminal
  * @n: number of times the character _ should be printed
+ * @i: length of line i.e. number of underscores
  */
 void print_line(int n)
 {
-	int c;
+	int i;
+	int j;
 
-	for (c = 0; n <= 4; c++)
+	for (i = 1; i <= n; i++)
 	{
-		for (n = 0; n <= 10; n++)
+		for (j = i; j >= 1; j--)
 		{
-			_putchar('_');
+			if (n > 0 && n <= 10)
+			{
+				j = j % 2;
+				_putchar('_');
+			}
+			else
+			{
+				_putchar('\n');
+			}
 		}
 	}
 	_putchar('\n');
