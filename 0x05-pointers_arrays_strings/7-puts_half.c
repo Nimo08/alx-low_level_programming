f5879354409aaef929baa2aa7304a9ae3651eea9
Nimo08 <nimomohamed800@gmail.com>
2023-05-02 04:11:46 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n = strlen(str);
+
+	for (i = n / 2; i < n; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
