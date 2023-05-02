44a2b9f8cb4b8a3de5b3f87a82c8112c7377da70
Nimo08 <nimomohamed800@gmail.com>
2023-05-02 02:13:36 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string(char *s) - reverses a string
+ * @s: pointer
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i;
+	char temp;
+	int len = strlen(s);
+	int mid = len / 2;
+
+	for (i = 0; i < mid; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i- 1] = temp;
+	}
+}
