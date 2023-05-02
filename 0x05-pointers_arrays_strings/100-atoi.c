69f5fb7a8e61c637572599017af4a7ed17bf256f
Nimo08 <nimomohamed800@gmail.com>
2023-05-02 06:39:45 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _atoi - convert string to integer
+ * @s: pointer
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int j = 0;
+	int x = 1;
+	int y = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			x *= -1;
+		}
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			y = 1;
+			x = (x * 10) + (s[i] - '0');
+			i++;
+		}
+		if (y == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	j *= x;
+	return (j);
+}
