1190912f9588ee00f88490251aae964d959cdcd2
Nimo08 <nimomohamed800@gmail.com>
2023-05-04 06:45:14 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints an int
+ * @n: int
+ * Return: int
+ */
+void print_number(int n)
+{
+	unsigned int j;
+
+	if (n < 0)
+	{
+		j = -n;
+		_putchar('-');
+	}
+	else
+	{
+		j = n;
+	}
+	if (j / 10)
+	{
+		print_number(j / 10);
+	}
+	_putchar((j % 10) + '0');
+}
