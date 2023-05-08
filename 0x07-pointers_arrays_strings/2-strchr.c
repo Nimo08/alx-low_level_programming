97980b49525bf535a17337364d969b4b34ad7b61
Nimo08 <nimomohamed800@gmail.com>
2023-05-08 03:09:59 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while ((*s != c) && (*s != '\0'))
 	{
-		if (*s == c)
-			return (s);
-		++s;
+		s++;
 	}
+	if (*s == c)
+		return (s);
 	return (NULL);
 }
