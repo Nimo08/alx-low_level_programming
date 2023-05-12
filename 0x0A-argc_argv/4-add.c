059a5dc0820405e85d243abff0757f798190ffcd
Nimo08 <nimomohamed800@gmail.com>
2023-05-12 02:52:25 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int res = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", res);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i][0]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res += atoi(argv[i]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
