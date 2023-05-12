7ac5b28a7f890e61c9c5797b7bc19f9271b54805
Nimo08 <nimomohamed800@gmail.com>
2023-05-12 03:03:49 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,20 +13,24 @@ int main(int argc, char *argv[])
 	int i;
 	int res = 0;
 
-	if (argc == 1)
+	if (argc > 0)
 	{
-		printf("%d\n", res);
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (!isdigit(argv[i][0]) && argv[i][0] != '-' && argv[i][0] != '+')
+
+		if (argc == 1)
 		{
-			printf("Error\n");
-			return (1);
+			printf("%d\n", res);
+			return (0);
 		}
-		res += atoi(argv[i]);
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(argv[i][0]) && argv[i][0] != '-' && argv[i][0] != '+')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			res += atoi(argv[i]);
+		}
+		printf("%d\n", res);
 	}
-	printf("%d\n", res);
 	return (0);
 }
