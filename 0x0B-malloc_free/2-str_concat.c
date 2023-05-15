22f0fb9538da7f9b2792867f293618f61b4377c2
Nimo08 <nimomohamed800@gmail.com>
2023-05-15 04:58:11 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,11 +18,12 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	s3 = (char *) malloc(len1 + len2 + 1);
+
 	if (s1 == NULL || s2 == NULL)
 	{
 		return (NULL);
 	}
+	s3 = (char *) malloc(len1 + len2 + 1);
 	if (s3 == NULL)
 	{
 		return (NULL);
