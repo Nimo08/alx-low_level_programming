93c17128cfeda7c4398e9100ca8a5d08af80943b
Nimo08 <nimomohamed800@gmail.com>
2023-05-18 14:53:48 +0300

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		new_ptr = malloc(sizeof(int));
+		return (malloc(new_size));
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
