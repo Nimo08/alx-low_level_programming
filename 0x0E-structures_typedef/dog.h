ffa77daa36fd08f8879f7a6337bf2d1f6babdd1e
Nimo08 <nimomohamed800@gmail.com>
2023-05-22 11:33:51 +0300

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new struct dog
+ * @name: char pointer
+ * @age: float  var
+ * @owner: char pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
