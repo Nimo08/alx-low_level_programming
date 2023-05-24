0bbd7688a10d6a5b6f7460fba647e3f09627aa3b
Nimo08 <nimomohamed800@gmail.com>
2023-05-24 19:03:46 +0300

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,24 +8,23 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned int count = 0;
-	unsigned int size;
+	int count;
+	int size;
 
+	size = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	size = atoi(argv[1]);
 	if (size < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	while (count < size)
+	for (count = 0; count < size; count++)
 	{
 		printf("%02x", *((unsigned char *)main + count));
-		count++;
 		if (size > count)
 		{
 			printf(" ");
