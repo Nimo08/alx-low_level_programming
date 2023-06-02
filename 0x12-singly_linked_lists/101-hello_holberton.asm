74433be09e65259c83c4ee01299baa371a9a9530
Nimo08 <nimomohamed800@gmail.com>
2023-06-02 16:02:29 +0300

    Task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,14 @@
 section	.text
 	global main
 main:
-	mov edx, len
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
-	int 0x80
-
-	mov eax, 1
-	int 0x80
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 18
+	syscall
+	mov rax, 60
+	xor rdi, rdi
+	syscall
 
 section	.data
 	msg db "Hello, Holberton", 10
-	len equ $ -msg
