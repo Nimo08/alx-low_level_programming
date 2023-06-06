5f161d68e27e6324dec67ff20edf73483a085b2f
Nimo08 <nimomohamed800@gmail.com>
2023-06-06 09:33:05 +0300

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,32 +8,24 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow_ptr;
-	const listint_t *fast_ptr;
+	const listint_t *ptr;
 	size_t cnt = 0;
 
 	if (head == NULL)
 	{
-		exit(98);
+		return (0);
 	}
-	slow_ptr = fast_ptr = head;
-	while (slow_ptr && fast_ptr && fast_ptr->next)
+	ptr = head;
+	while (ptr != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+		printf("[%p] %d\n", (void *)ptr, ptr->n);
 		cnt++;
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
-		if (slow_ptr == fast_ptr)
+		if (ptr <= ptr->next && ptr->next != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			printf("-> [%p] %d\n", (void *)ptr, ptr->n);
 			exit(98);
 		}
-	}
-	while (slow_ptr != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
-		cnt++;
-		slow_ptr = slow_ptr->next;
+		ptr = ptr->next;
 	}
 	return (cnt);
 }
