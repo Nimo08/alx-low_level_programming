c20077b66f327653da8ad9d025335ff78c29cbb2
Nimo08 <nimomohamed800@gmail.com>
2023-06-08 14:08:44 +0300

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,23 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int bit_mask = 32768;
+	int zeros = 1;
 
+	if (n == 0)
+	{
+		putchar('0');
+	}
 	while (bit_mask > 0)
 	{
-		if ((n & bit_mask) == 0)
-			putchar('0');
-		else
+		if ((n & bit_mask) != 0)
+		{
+			zeros = 0;
 			putchar('1');
+		}
+		else if (!zeros)
+		{
+			putchar('0');
+		}
 		bit_mask = bit_mask >> 1;
 	}
 }
