7c9c0b257bac5324cd585e980c23ca027fcf332a
Nimo08 <nimomohamed800@gmail.com>
2023-06-08 16:05:15 +0300

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * flip_bits - return number of bits you would need to flip to get from
  * one number to another
@@ -9,14 +8,10 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int val;
-	unsigned int cnt = 0;
+	unsigned long int val;
+	unsigned long int cnt = 0;
 
 	val = n ^ m;
-	if (val == 0)
-	{
-		return (-1);
-	}
 	while (val)
 	{
 		val = val & (val - 1);
