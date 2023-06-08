02fdbf08d74398e7cc7072c267c63b6c920121f3
Nimo08 <nimomohamed800@gmail.com>
2023-06-08 15:16:51 +0300

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index < sizeof(unsigned long int) * 8)
 	{
-		*n |= (1UL << index);
+		*n |= (1 << index);
+		return (1);
 	}
 	return (-1);
 }
