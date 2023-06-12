4c6b34a055771b3ec56cf56069ef47b1d391c6d6
Nimo08 <nimomohamed800@gmail.com>
2023-06-12 22:45:30 +0300

    Added file_copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -48,42 +47,37 @@ void close_error(int file)
 	exit(100);
 }
 /**
- * main - copies content of a file to another file
- * @ac: argument count
- * @av: argument vector
- * Return: 0
+ * file_copy - copies content of file1 to file2
+ * @file1: file to be copied from
+ * @file2: file to get the copied content
  */
-int main(int ac, char **av)
+void file_copy(const char *file1, const char *file2)
 {
 	int file_to, file_from;
 	char buf[BUF];
 	int read_data = 0, write_data = 0;
 
-	if (ac != 3)
-	{
-		error_usage();
-	}
-	file_from = open(av[1], O_RDONLY);
+	file_from = open(file1, O_RDONLY);
 	if (file_from == -1)
 	{
-		read_error(av[1]);
+		read_error(file1);
 	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_to = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		write_error(av[2]);
+		write_error(file2);
 	}
 	while ((read_data = read(file_from, buf, BUF)) > 0)
 	{
 		write_data = write(file_to, buf, read_data);
 		if (write_data == -1 || write_data != read_data)
 		{
-			write_error(av[2]);
+			write_error(file2);
 		}
 	}
 	if (read_data == -1)
 	{
-		read_error(av[1]);
+		read_error(file1);
 	}
 	if (close(file_from) == -1)
 	{
@@ -93,5 +87,20 @@ int main(int ac, char **av)
 	{
 		close_error(file_to);
 	}
+}
+/**
+ * main - copies content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		error_usage();
+	}
+	file_copy(av[1], av[2]);
+
 	return (0);
 }
