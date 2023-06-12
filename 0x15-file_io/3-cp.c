bc7051ea077dea4ad8ff46a98dfb59a454309511
Nimo08 <nimomohamed800@gmail.com>
2023-06-12 19:28:25 +0300

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,6 +7,17 @@
 #include <sys/stat.h>
 #define BUF 1024
 /**
+ * exit_error - exits with an error
+ * @msg: error message
+ * @av1: arg 1
+ * @code: exit code
+ */
+void exit_error(const char *msg, const char *av1, int code)
+{
+	dprintf(STDERR_FILENO, msg, av1);
+	exit(code);
+}
+/**
  * main - copies content of a file to another file
  * @ac: argument count
  * @av: argument vector
@@ -20,38 +31,32 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s cp file_from file_to\n", av[0]);
-		exit(97);
+		exit_error("Usage: %s cp file_from file_to\n", av[0], 97);
 	}
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		exit_error("Error: Can't read from file %s\n", av[1], 98);
 	}
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-		exit(99);
+		exit_error("Error: Can't write to file %s\n", av[2], 99);
 	}
 	while ((read_data = read(file_from, buf, BUF)) > 0)
 	{
 		if (write(file_to, buf, read_data) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-			exit(99);
+			exit_error("Error: Can't write to file %s\n", av[2], 99);
 		}
 	}
 	if (read_data == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't read from file %s\n",  av[1]);
-		exit(98);
+		exit_error("Can't read from file %s\n",  av[1], 98);
 	}
 	if (close(file_from) == -1 || close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd value\n");
-		exit(100);
+		exit_error("Error: Can't close fd value\n", "", 100);
 	}
 	return (0);
 }
