0a0790885835dffc2bff968c8c0caa91875e815b
Nimo08 <nimomohamed800@gmail.com>
2023-06-12 15:02:47 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,26 +15,20 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	int len;
+	int len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
-		chmod(filename, 0600);
-	}
-	else
-	{
-		len = strlen(text_content);
-		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
-		chmod(filename, 0600);
-	}
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
 		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
 	}
 	if (write(file, text_content, len) == -1)
 	{
