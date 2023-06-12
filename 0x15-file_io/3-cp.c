b00803869aa2a486ffc40a98320f3859949f4589
Nimo08 <nimomohamed800@gmail.com>
2023-06-12 21:07:13 +0300

    Added functions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,32 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #define BUF 1024
+void exit_error(const char *msg, const char *av1, int code);
+int file1(const char *filename);
+int file2(const char *filename);
+void file_copy(int f1, int f2);
+void file_close(int file);
+/**
+ * main - copies content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int file_to, file_from;
+
+	if (ac != 3)
+	{
+		exit_error("Usage: %s cp file_from file_to\n", av[0], 97);
+	}
+	file_from = file1(av[1]);
+	file_to = file2(av[2]);
+	file_copy(file_from, file_to);
+	file_close(file_from);
+	file_close(file_to);
+	return (0);
+}
 /**
  * exit_error - exits with an error
  * @msg: error message
@@ -18,51 +44,70 @@ void exit_error(const char *msg, const char *av1, int code)
 	exit(code);
 }
 /**
- * main - copies content of a file to another file
- * @ac: argument count
- * @av: argument vector
- * Return: 0
+ * file1 - opens first file
+ * @filename: file name
+ * Return: file_from
  */
-int main(int ac, char **av)
+int file1(const char *filename)
 {
-	int file_to, file_from;
-	char buf[BUF];
-	int read_data = 0, write_data = 0;
+	int f1;
 
-	if (ac != 3)
+	f1 = open(filename, O_RDONLY);
+	if (f1 == -1)
 	{
-		exit_error("Usage: %s cp file_from file_to\n", av[0], 97);
+		exit_error("Error: Can't read from file %s\n", filename, 98);
 	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
+	return (f1);
+}
+/**
+ * file2 - opens second file
+ * @filename: file name
+ * Return: file_to
+ */
+int file2(const char *filename)
+{
+	int f2;
+
+	f2 = open(filename, O_WRONLY, O_CREAT | O_TRUNC, 0664);
+	if (f2 == -1)
 	{
-		exit_error("Error: Can't read from file %s\n", av[1], 98);
+		exit_error("Error: Can't write to file %s\n", filename, 99);
 	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
+	return (f2);
+}
+/**
+ * file_copy - copies f1 to f2
+ * @f1: source file
+ * @f2: destination file
+ * Return: nothing
+ */
+void file_copy(int f1, int f2)
+{
+	char buf[BUF];
+	int read_data, write_data;
+
+	while ((read_data = read(f1, buf, BUF)) > 0)
 	{
-		close(file_from);
-		exit_error("Error: Can't write to file %s\n", av[2], 99);
-	}
-	while ((read_data = read(file_from, buf, BUF)) > 0)
-	{
-		write_data = write(file_to, buf, read_data);
+		write_data = write(f2, buf, read_data);
 		if (write_data == -1 || write_data != read_data)
 		{
-			exit_error("Error: Can't write to file %s\n", av[2], 99);
+			exit_error("Error: Can't write to file\n", "", 99);
 		}
 	}
 	if (read_data == -1)
 	{
-		exit_error("Can't read from file %s\n", av[1], 98);
+		exit_error("Can't read from file\n", "", 98);
 	}
-	if (close(file_from) == -1)
+}
+/**
+ * file_close - closes file
+ * @file: file
+ * Return: nothing
+ */
+void file_close(int file)
+{
+	if (close(file) == -1)
 	{
 		exit_error("Error: Can't close fd value\n", "", 100);
 	}
-	if (close(file_to) == -1)
-	{
-		exit_error("Error: Can't close fd value\n", "", 100);
-	}
-	return (0);
 }
