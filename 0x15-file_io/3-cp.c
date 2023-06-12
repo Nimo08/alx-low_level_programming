c68ff047dad6350b5e890c5c5ad872638770c063
Nimo08 <nimomohamed800@gmail.com>
2023-06-12 22:24:19 +0300

    Added errorfunctions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,47 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #define BUF 1024
-void exit_error(const char *msg, const char *av1, int code);
+void error_usage(void);
+void read_error(const char *filename);
+void write_error(const char *filename);
+void close_error(int file);
+/**
+ * error_usage - displays usage error message
+ */
+void error_usage(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
+ * read_error - read error message
+ * @filename: name of file
+ * Return: nothing
+ */
+void read_error(const char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
+}
+/**
+ * write_error - write error message
+ * @filename: name of file
+ * Return: nothing
+ */
+void write_error(const char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
+	exit(99);
+}
+/**
+ * close_error - close error message
+ * @file: file
+ */
+void close_error(int file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd value %d\n", file);
+	exit(100);
+}
 /**
  * main - copies content of a file to another file
  * @ac: argument count
@@ -21,44 +61,37 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		exit_error("Usage: %s cp file_from file_to\n", av[0], 97);
+		error_usage();
 	}
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		exit_error("Error: Can't read from file %s\n", av[1], 98);
+		read_error(av[1]);
 	}
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		exit_error("Error: Can't write to file %s\n", av[2], 99);
+		write_error(av[2]);
 	}
 	while ((read_data = read(file_from, buf, BUF)) > 0)
 	{
 		write_data = write(file_to, buf, read_data);
 		if (write_data == -1 || write_data != read_data)
 		{
-			exit_error("Error: Can't write to file %s\n", av[2], 99);
+			write_error(av[2]);
 		}
 	}
 	if (read_data == -1)
 	{
-		exit_error("Can't read from file %s\n", av[1], 98);
+		read_error(av[1]);
 	}
-	if ((close(file_from) == -1) || (close(file_to) == -1))
+	if (close(file_from) == -1)
 	{
-		exit_error("Error: Can't close fd value\n", "", 100);
+		close_error(file_from);
+	}
+	if (close(file_to) == -1)
+	{
+		close_error(file_to);
 	}
 	return (0);
-}
-/**
- * exit_error - exits with an error
- * @msg: error message
- * @av1: arg 1
- * @code: exit code
- */
-void exit_error(const char *msg, const char *av1, int code)
-{
-	dprintf(STDERR_FILENO, msg, av1);
-	exit(code);
 }
