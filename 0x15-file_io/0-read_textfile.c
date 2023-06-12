2faaaf63c74db3abbf458d64d44608c958bfdd40
Nimo08 <nimomohamed800@gmail.com>
2023-06-12 12:15:36 +0300

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: file name
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *read_ptr;
+	char *letters_buf;
+	size_t num_letters = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	read_ptr = fopen(filename, "r");
+	if (read_ptr != NULL)
+	{
+		letters_buf = malloc(letters + 1);
+		if (letters_buf == NULL)
+		{
+			return (0);
+		}
+		while (fgets(letters_buf, letters + 1, read_ptr) != NULL)
+		{
+			write(STDOUT_FILENO, letters_buf, strlen(letters_buf));
+			num_letters += strlen(letters_buf);
+		}
+		fclose(read_ptr);
+		free(letters_buf);
+		return (num_letters);
+	}
+	return (0);
+}
