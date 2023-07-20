b3a382a36bd0d6da03fd8020fed1e7442ee27fd8
Nimo08 <nimomohamed800@gmail.com>
2023-07-20 15:56:09 +0300

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr = ptr->next;
 		idx--;
 	}
-	if (idx > 0)
+	if (ptr == NULL)
 	{
 		free(temp);
 		return (NULL);
