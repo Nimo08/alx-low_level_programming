f4def48e36f5f09f998de3192d80438f2d7b0cea
Nimo08 <nimomohamed800@gmail.com>
2023-07-24 13:54:55 +0300

    Task 3

--- 0x18-dynamic_libraries/101-rand.c
+++ 0x18-dynamic_libraries/101-rand.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * _rand - replaces rand with _rand
  * Return: specific numbers to win
  */
 int _rand(void)
 {
-	int i;
+	int i = rand();
 
-	if (i != 9 || i != 8 || i != 10 || i != 24 || i != 75)
-	{
-		printf("Sorry, try again!\n");
-	}
-	else
+	if (i == 9 || i == 8 || i == 10 || i == 24 || i == 75)
 	{
 		printf("--> Please make me win!\n");
 	}
-	return (0);
+	else
+	{
+		printf("Sorry, try again!\n");
+	}
+	return (i);
 }
