75487ac5bca994fb65f0f6b45eb28f0cc66fe27a
Nimo08 <nimomohamed800@gmail.com>
2023-07-28 14:18:23 +0300

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Nimo08/alx-low_level_programming/master/0x18-dynamic_libraries/libprint.so
+wget -P .. https://github.com/Nimo08/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libprint.so
 export LD_PRELOAD="$PWD/../libprint.so"
