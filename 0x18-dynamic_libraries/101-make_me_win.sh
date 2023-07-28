508b08e938661a6b172ebd22e2c2e116e1a54ba5
Nimo08 <nimomohamed800@gmail.com>
2023-07-28 09:10:02 +0300

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-wget -P .. https://github.com/Nimo08/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmake_me_win.so
-export LD_PRELOAD="$PWD/../libmake_me_win.so"
+#!/bin/bwget -P .. https://github.com/Nimo08/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmake_me_win.so
+wget -P .. https://github.com/Nimo08/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmake_me_win.so
