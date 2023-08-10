95614851cab966d7ec93f8eb49249644f313b2f5
Nimo08 <nimomohamed800@gmail.com>
2023-08-10 21:19:58 +0300

    Added ht check for null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	if (ht->array[index] != NULL)
 	{
