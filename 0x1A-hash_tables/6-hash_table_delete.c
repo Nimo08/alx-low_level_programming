79bcab8c14bf798fc745e0fdde470cbe14596b9a
Nimo08 <nimomohamed800@gmail.com>
2023-08-11 14:27:16 +0300

    Task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * free_node - frees the node
+ * @node: pointer
+ * Return: nothing
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *ptr, *temp;
+	unsigned long int index = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	for (index = 0; index < ht->size; index++)
+	{
+		ptr = ht->array[index];
+		while (ptr != NULL)
+		{
+			temp = ptr->next;
+			free_node(ptr);
+			ptr = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
